324bb83e84c2ed245394a249c4c94355d62d6921
irian <irian.code@hotmail.com>
2023-06-30 00:01:18 +0200

    Initial files with dummy PDF rendering

---
+++ src/pages/cv.pdf.ts
@@ -0,0 +1,23 @@
+import type {APIRoute} from "astro";
+import ReactPDF from "@react-pdf/renderer";
+import TestPdf from "../components/cv/TestPdf"
+
+export const get: APIRoute = async function get ({params, request}) {
+    const pdfStream = await ReactPDF.renderToStream(TestPdf())
+
+    // Reading the stream
+    const chunks= [];
+    for await (let chunk of pdfStream) {
+        chunks.push(chunk);
+    }
+
+    //@ts-expect-error It doesn't like that it's not typed correctly, but this works.
+    const buffer = Buffer.concat(chunks);
+
+    // Serializing to string
+    // Inspiration: https://github.com/withastro/astro/blob/255636cc7b4ed5f72045f75a2411ebd84a2bdb0d/packages/astro/test/fixtures/non-html-pages/src/pages/placeholder.png.ts
+    return {
+        body: buffer.toString('binary'),
+        encoding: 'binary'
+    };
+}
