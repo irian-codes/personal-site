0d44761b69d3d0e8587f205205d32702b995b3e9
irian <irian.code@hotmail.com>
2023-09-20 18:26:41 +0200

    Simplified returning PDF file
    With Astro 3.0 we can simplify

--- src/pages/cv.pdf.ts
+++ src/pages/cv.pdf.ts
@@ -2,7 +2,7 @@ import ReactPDF from '@react-pdf/renderer';
 import type {APIRoute} from 'astro';
 import TestPdf from '../components/cv/TestPdf';
 
-export const get: APIRoute = async function get({params, request}) {
+export const GET: APIRoute = async function (context) {
   // TODO: Move this implementation to function 'renderToBuffer' when it's implemented in Typescript in the library.
   const pdfStream = await ReactPDF.renderToStream(TestPdf());
 
@@ -15,10 +15,5 @@ export const get: APIRoute = async function get({params, request}) {
   //@ts-expect-error It doesn't like that it's not typed correctly, but this works.
   const buffer = Buffer.concat(chunks);
 
-  // Serializing to string
-  // Inspiration: https://github.com/withastro/astro/blob/255636cc7b4ed5f72045f75a2411ebd84a2bdb0d/packages/astro/test/fixtures/non-html-pages/src/pages/placeholder.png.ts
-  return {
-    body: buffer.toString('binary'),
-    encoding: 'binary',
-  };
+  return new Response(buffer);
 };
