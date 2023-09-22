3c9c9612e128bf153bb155228c65776e1d41baae
irian <irian.code@hotmail.com>
2023-09-22 15:58:21 +0200

    Created very basic made up resume to test
    I want to test it with different resume checkers online
    and also different PDF readers to ensure this library is
    compatible.

--- src/components/cv/TestPdf.tsx
+++ src/components/cv/TestPdf.tsx
@@ -1,30 +1,90 @@
 import {Document, Page, StyleSheet, Text, View} from '@react-pdf/renderer';
 
-// Create Document Component
 export default function TestPdf() {
   return (
     <Document>
       <Page size="A4" style={styles.page}>
         <View style={styles.section}>
-          <Text>Section #1</Text>
+          <Text style={styles.name}>John Doe</Text>
+          <Text style={styles.content}>Web Developer</Text>
         </View>
+
         <View style={styles.section}>
-          <Text>Section #2</Text>
+          <Text style={styles.heading}>Summary</Text>
+          <Text style={styles.content}>
+            Highly skilled web developer with 5 years of experience in building
+            responsive and user-friendly websites. Proficient in HTML, CSS,
+            JavaScript, and React. Strong problem-solving and communication
+            skills.
+          </Text>
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.heading}>Education</Text>
+          <Text style={styles.content}>
+            Bachelor of Science in Computer Science, XYZ University
+          </Text>
+          <Text style={styles.content}>Graduated in 2015</Text>
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.heading}>Experience</Text>
+          <Text style={styles.content}>
+            Web Developer, ABC Company (2016 - Present)
+          </Text>
+          <Text style={styles.content}>
+            - Developed and maintained company websites using React and Node.js
+          </Text>
+          <Text style={styles.content}>
+            - Collaborated with cross-functional teams to deliver high-quality
+            projects
+          </Text>
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.heading}>Skills</Text>
+          <Text style={styles.content}>
+            - HTML5, CSS3, JavaScript, React, Node.js
+          </Text>
+          <Text style={styles.content}>- Responsive Web Design</Text>
+          <Text style={styles.content}>- Version Control (Git)</Text>
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.heading}>Other</Text>
+          <Text style={styles.content}>Languages Spoken: English, Spanish</Text>
+          <Text style={styles.content}>Driving License: Yes</Text>
+          <Text style={styles.content}>
+            Invented Award: Outstanding Innovator of the Year
+          </Text>
         </View>
       </Page>
     </Document>
   );
 }
 
-// Create styles
 const styles = StyleSheet.create({
   page: {
-    flexDirection: 'row',
-    backgroundColor: '#E4E4E4',
+    flexDirection: 'column',
+    backgroundColor: '#ffffff',
+    padding: '1cm',
   },
   section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1,
+    marginBottom: '0.5cm',
+  },
+  name: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: '0.3cm',
+  },
+  heading: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginBottom: '0.3cm',
+    textDecoration: 'underline',
+  },
+  content: {
+    fontSize: 12,
+    marginBottom: '0.2cm',
   },
 });
