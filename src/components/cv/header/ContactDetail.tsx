4583b3bf77e6bf59285622ec4ae8e527c4989e3d
irian <irian.code@hotmail.com>
2023-09-22 19:40:45 +0200

    WIP: Adding a small icon to the left of contact details

--- src/components/cv/header/ContactDetail.tsx
+++ src/components/cv/header/ContactDetail.tsx
@@ -1,4 +1,4 @@
-import {StyleSheet, Text, View} from '@react-pdf/renderer';
+import {Image, StyleSheet, Text, View} from '@react-pdf/renderer';
 
 export type ContactType = 'email' | 'location' | 'linkedin' | 'github';
 type ContactDetailProps = {
@@ -9,14 +9,36 @@ type ContactDetailProps = {
 export const ContactDetail = (props: ContactDetailProps) => {
   return (
     <View style={[styles.container, props.containerStyle]}>
-      <Text style={{fontWeight: 'bold', fontSize: 12}}>Lorem</Text>
-      <Text style={{fontSize: 10}}>Lorem ipsum</Text>
+      <Image
+        src={'http://localhost:4321/assets/images/dummy-cv-image.jpg'}
+        style={styles.icon}
+      />
+      <View style={styles.textContainer}>
+        <Text style={styles.label}>Lorem</Text>
+        <Text style={styles.text}>Lorem ipsum</Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'column',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    width: '1cm',
+    height: '1cm',
+    marginRight: '0.3cm',
+  },
+  textContainer: {
+    flex: 1,
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: 12,
+  },
+  text: {
+    fontSize: 10,
   },
 });
