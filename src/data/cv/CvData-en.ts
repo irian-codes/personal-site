b19aa497198f0d3e1e36e177c9db5b897766e3ac
irian <irian.code@hotmail.com>
2023-10-13 18:54:43 +0200

    Added protocol to website
    This is because some people may not perceive it as such

--- src/data/cv/CvData-en.ts
+++ src/data/cv/CvData-en.ts
@@ -16,7 +16,7 @@ const header: Header = {
   birthdate: '1993-12-10T00:00:00+01:00',
   // TODO: Set the correct URL on deployment.
   photoSrc: 'http://localhost:4321/assets/images/cv/irian-cv-image_2022.jpeg',
-  websiteUrl: 'irian.codes',
+  websiteUrl: 'https://irian.codes',
   // TODO: Set the correct URL on deployment.
   websiteQrImageSrc:
     'http://localhost:4321/assets/images/cv/irian-codes-qr.png',
