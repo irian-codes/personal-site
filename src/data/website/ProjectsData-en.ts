54d5188624bc57f60bf7ee3685c981dab72c74dc
irian <irian.code@hotmail.com>
2023-11-07 21:12:16 +0100

    Added some real project images

--- src/data/website/ProjectsData-en.ts
+++ src/data/website/ProjectsData-en.ts
@@ -1,16 +1,20 @@
 import reactLogoSvg from '../../assets/images/icons/react-logo.svg';
-import medwayMobileAppImg from '../../assets/images/projects/medway/medway-joined.jpg';
+import assistantDriversAppImg from '../../assets/images/projects/mobile-apps/assistant-drivers-joined.jpg';
+import caummaMobileAppImg from '../../assets/images/projects/mobile-apps/caumma-joined.jpg';
+import cordobaVivaMobileAppImg from '../../assets/images/projects/mobile-apps/cordoba-viva-joined.jpg';
+import deliveryKingsMobileAppImg from '../../assets/images/projects/mobile-apps/delivery-kings-joined.jpg';
+import medwayMobileAppImg from '../../assets/images/projects/mobile-apps/medway-joined.jpg';
 import type {Projects} from './types/ProjectData';
 
 export const projectsData_en: Projects = [
-  // TODO: These are dummy projects, put real projects
+  // TODO: Add more real projects
+  // TODO: Complete the missing information replacing dummy text
   {
     id: 0,
-    name: 'Medway App',
+    name: 'Caumma',
     description:
       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia natus libero voluptas, eum corporis iste.',
-    image: medwayMobileAppImg,
-    // TODO: Put the correct logo images
+    image: caummaMobileAppImg,
     technologies: [
       {
         name: 'React Native',
@@ -28,11 +32,73 @@ export const projectsData_en: Projects = [
   },
   {
     id: 1,
-    name: 'Medway App',
+    name: 'Mediterranean Way',
     description:
       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia natus libero voluptas, eum corporis iste.',
     image: medwayMobileAppImg,
-    // TODO: Put the correct logo images
+    technologies: [
+      {
+        name: 'React Native',
+        icon: reactLogoSvg,
+      },
+      {
+        name: 'Android',
+        icon: reactLogoSvg,
+      },
+      {
+        name: 'iOS',
+        icon: reactLogoSvg,
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Assistant Drivers',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia natus libero voluptas, eum corporis iste.',
+    image: assistantDriversAppImg,
+    technologies: [
+      {
+        name: 'React Native',
+        icon: reactLogoSvg,
+      },
+      {
+        name: 'Android',
+        icon: reactLogoSvg,
+      },
+      {
+        name: 'iOS',
+        icon: reactLogoSvg,
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: 'Delivery Kings',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia natus libero voluptas, eum corporis iste.',
+    image: deliveryKingsMobileAppImg,
+    technologies: [
+      {
+        name: 'React Native',
+        icon: reactLogoSvg,
+      },
+      {
+        name: 'Android',
+        icon: reactLogoSvg,
+      },
+      {
+        name: 'iOS',
+        icon: reactLogoSvg,
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: 'Córdoba Viva',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia natus libero voluptas, eum corporis iste.',
+    image: cordobaVivaMobileAppImg,
     technologies: [
       {
         name: 'React Native',
