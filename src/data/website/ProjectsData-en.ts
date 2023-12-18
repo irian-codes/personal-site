8b524fc88db67472c96fa39d4a03084633199c70
irian <irian.code@hotmail.com>
2023-12-18 19:19:29 +0100

    Formatted code without innecessary newlines

--- src/data/website/ProjectsData-en.ts
+++ src/data/website/ProjectsData-en.ts
@@ -80,24 +80,12 @@ export const projectsData_en: Projects = [
       'Mobile app for a psychology clinic to help therapists manage their patients. Also, the patients have an account so they can log their homework tasks.',
     images: projectsGalleries.get('caumma') || [],
     technologies: [
-      {
-        name: 'React Native',
-      },
-      {
-        name: 'Typescript',
-      },
-      {
-        name: 'Expo',
-      },
-      {
-        name: 'Rest API',
-      },
-      {
-        name: 'Android',
-      },
-      {
-        name: 'iOS',
-      },
+      {name: 'React Native'},
+      {name: 'Typescript'},
+      {name: 'Expo'},
+      {name: 'Rest API'},
+      {name: 'Android'},
+      {name: 'iOS'},
     ],
   },
   {
@@ -107,21 +95,11 @@ export const projectsData_en: Projects = [
       'Mobile app to serve an Uber-like business. Customers can hire a driver by the hour or by trip distance and get billed in the app for the exact amount. The client requested one app for users and another for his drivers.',
     images: projectsGalleries.get('assistant-drivers-mobile') || [],
     technologies: [
-      {
-        name: 'React Native',
-      },
-      {
-        name: 'Expo',
-      },
-      {
-        name: 'Typescript',
-      },
-      {
-        name: 'Firebase',
-      },
-      {
-        name: 'Android',
-      },
+      {name: 'React Native'},
+      {name: 'Expo'},
+      {name: 'Typescript'},
+      {name: 'Firebase'},
+      {name: 'Android'},
     ],
   },
   {
@@ -131,18 +109,11 @@ export const projectsData_en: Projects = [
       'Mobile app to serve an Uber-like business. Customers can hire a driver by the hour or by trip distance and get billed in the app for the exact amount. The client requested one app for users and another for his drivers.',
     images: projectsGalleries.get('assistant-drivers-desktop') || [],
     technologies: [
-      {
-        name: 'React',
-      },
-      {
-        name: 'Material UI',
-      },
-      {
-        name: 'Typescript',
-      },
-      {
-        name: 'Firebase',
-      },
+      {name: 'React'},
+      {name: 'Material UI'},
+      {name: 'Typescript'},
+      {name: 'Firebase'},
+      {name: 'Rest API'},
     ],
   },
   {
@@ -152,21 +123,11 @@ export const projectsData_en: Projects = [
       'Mobile app for managing tourist apartments, where reservations and room status can be visualized, and each cleaning round can be registered and recorded with photos of any incidents. It also has functionality to calculate the revenue per room for the hotel manager.',
     images: projectsGalleries.get('medway') || [],
     technologies: [
-      {
-        name: 'React Native',
-      },
-      {
-        name: 'Typescript',
-      },
-      {
-        name: 'Rest API',
-      },
-      {
-        name: 'Android',
-      },
-      {
-        name: 'iOS',
-      },
+      {name: 'React Native'},
+      {name: 'Typescript'},
+      {name: 'Rest API'},
+      {name: 'Android'},
+      {name: 'iOS'},
     ],
   },
   {
@@ -176,21 +137,11 @@ export const projectsData_en: Projects = [
       'Mobile app for on-demand delivery and errand services. Users can buy almost any item, from food, medication or even a custom order with specific instructions. They can also send a package to another user after filling a form including photos.',
     images: projectsGalleries.get('delivery-kings') || [],
     technologies: [
-      {
-        name: 'React Native',
-      },
-      {
-        name: 'Typescript',
-      },
-      {
-        name: 'Rest API',
-      },
-      {
-        name: 'Android',
-      },
-      {
-        name: 'iOS',
-      },
+      {name: 'React Native'},
+      {name: 'Typescript'},
+      {name: 'Rest API'},
+      {name: 'Android'},
+      {name: 'iOS'},
     ],
   },
   {
@@ -200,21 +151,11 @@ export const projectsData_en: Projects = [
       'Mobile app for users to check the catalogue of tourist guided tours in the city of Córdoba, Spain. The client is a travel agency. Users can check updated dates and times of all events and make a reservation.',
     images: projectsGalleries.get('cordoba-viva') || [],
     technologies: [
-      {
-        name: 'React Native',
-      },
-      {
-        name: 'Typescript',
-      },
-      {
-        name: 'Rest API',
-      },
-      {
-        name: 'Android',
-      },
-      {
-        name: 'iOS',
-      },
+      {name: 'React Native'},
+      {name: 'Typescript'},
+      {name: 'Rest API'},
+      {name: 'Android'},
+      {name: 'iOS'},
     ],
   },
 ];
