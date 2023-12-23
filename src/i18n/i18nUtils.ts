irian-codes/personal-site

commit 97b7181a60f63ac079facc5447b7766ea5ba2912

Correctly redirecting to the language page

src/i18n/i18nUtils.ts
@@ -1,4 +1,9 @@
-import {defaultLanguageTag, translations, type LanguageTag} from './i18n';
+import {
+  defaultLanguageTag,
+  languages,
+  translations,
+  type LanguageTag,
+} from './i18n';
 
 /**
  * Translation hook to load i18n translations from JSON.
@@ -36,7 +41,13 @@ export function getLangFromUrl(url: URL): LanguageTag {
  */
 export function switchUrlLang(url: URL, lang: LanguageTag): URL {
   const pathSegments = url.pathname.split('/');
-  pathSegments[1] = lang;
+
+  // find the current language tag in pathSegments
+  const index = pathSegments.findIndex((segment) =>
+    Object.keys(languages).some((langTag) => langTag === segment)
+  );
+
+  pathSegments[index] = lang;
   const newUrl = new URL(url.origin + pathSegments.join('/'));
 
   return newUrl;

commit ffe01a50f1e4b109eb7d533d1d0060638c8000b7

Fixed bug introduced in 97b7181a60f63ac079facc5447b7766ea5ba2912
Now the language is detected properly.

src/i18n/i18nUtils.ts
@@ -27,7 +27,9 @@ export function useTranslations(lang: LanguageTag) {
  */
 export function getLangFromUrl(url: URL): LanguageTag {
   const pathSegments = url.pathname.split('/');
-  const langTag = pathSegments[1] as LanguageTag;
+  const langTag = pathSegments.find((segment) =>
+    Object.keys(languages).some((langTag) => langTag === segment)
+  ) as LanguageTag;
 
   return langTag in translations ? langTag : defaultLanguageTag;
 }