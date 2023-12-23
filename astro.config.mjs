4f7dea266bc743eebf4c171ee9230b5586b12aca
irian <irian.code@hotmail.com>
2023-12-23 03:16:40 +0100

    Adding params to make it apparent that they could be used

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import {defineConfig} from 'astro/config';
 import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({
+export default defineConfig(({command, mode}) => ({
   integrations: [react()],
   site: 'https://irian-codes.github.io',
   base: '/personal-site',
-});
+}));
