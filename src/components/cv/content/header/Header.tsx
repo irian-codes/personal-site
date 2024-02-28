30a60bfb5c9d61e3f0aa70b278fdf1edb01b7dd3
irian <irian.code@hotmail.com>
2024-02-28 20:16:59 +0100

    Added conditional image to CV and adjusted spacing

--- src/components/cv/content/header/Header.tsx
+++ src/components/cv/content/header/Header.tsx
@@ -1,4 +1,4 @@
-import {StyleSheet, Text, View} from '@react-pdf/renderer';
+import {Image, StyleSheet, Text, View} from '@react-pdf/renderer';
 import {useContext} from 'react';
 import {cvData} from '../../../../data/cv/CvData';
 import {LanguageContext} from '../../CvPdf';
@@ -11,18 +11,37 @@ export const Header = () => {
   const langTag = useContext(LanguageContext);
   const data = cvData.data.find((entry) => entry.langTag === langTag)!;
 
+  const ApplicantImageComponent = () => {
+    if (data.content.header.photoSrc.length > 0) {
+      return (
+        <Image
+          src={data.content.header.photoSrc}
+          style={styles.applicantPhoto}
+        />
+      );
+    } else {
+      return null;
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.firstColumnContainer}>
-        <Text style={styles.name}>
-          {data.content.header.name + ' ' + data.content.header.surnames}
-        </Text>
-        <Text style={styles.position}>{data.content.header.position}</Text>
+        <View style={styles.photoAndTextContainer}>
+          <ApplicantImageComponent />
+
+          <View>
+            <Text style={styles.name}>
+              {data.content.header.name + ' ' + data.content.header.surnames}
+            </Text>
+            <Text style={styles.position}>{data.content.header.position}</Text>
+          </View>
+        </View>
         <AboutMeSection />
       </View>
 
       {/* spacer */}
-      {<View style={{width: headerStyles.spacing.big}} />}
+      {<View style={{width: headerStyles.spacing.medium}} />}
 
       <View style={styles.secondColumnContainer}>
         <ContactDetail type={'email'} containerStyle={styles.contactDetail} />
@@ -50,22 +69,26 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'flex-start',
-    padding: headerStyles.spacing.small,
-    paddingTop: headerStyles.spacing.smallest,
+    marginBottom: headerStyles.spacing.medium,
   },
   firstColumnContainer: {
     flexDirection: 'column',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     alignItems: 'flex-start',
-    width: '100%',
+    width: '15cm',
   },
   secondColumnContainer: {
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    width: '12cm',
+    width: '8cm',
     flexWrap: 'wrap',
     height: '5cm',
+  },
+  photoAndTextContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignContent: 'center',
   },
   name: {
     fontSize: headerStyles.text.fontSize.name,
@@ -76,8 +99,9 @@ const styles = StyleSheet.create({
   position: {
     fontSize: 18,
     fontStyle: 'italic',
-    textAlign: 'center',
+    textAlign: 'left',
     marginTop: '-0.2cm',
+    maxWidth: '8.5cm',
     marginBottom: headerStyles.spacing.small,
     fontFamily: headerStyles.text.fontFamily.name,
     color: cvStyles.colors.secondary,
@@ -89,7 +113,7 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 10,
     borderBottomLeftRadius: 10,
     borderBottomRightRadius: 10,
-    alignSelf: 'center',
+    marginRight: headerStyles.spacing.small,
   },
   qrContainer: {
     flexDirection: 'column',
