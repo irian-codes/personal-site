09fa396bbc925b953c27d9193020e6edba51c542
irian <irian.code@hotmail.com>
2024-03-12 19:41:47 +0100

    Installed and configured ESLint

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:astro/recommended'],
+  overrides: [
+    {
+      // Define the configuration for `.astro` file.
+      files: ['*.astro'],
+      // Allows Astro components to be parsed.
+      parser: 'astro-eslint-parser',
+      // Parse the script in `.astro` as TypeScript by adding the following configuration.
+      // It's the setting you need when using TypeScript.
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+      rules: {
+        // override/add rules settings here, such as:
+        // "astro/no-set-html-directive": "error"
+      },
+    },
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      extends: ['plugin:@typescript-eslint/recommended'],
+      rules: {
+        // override/add rules settings here, such as:
+        // "astro/no-set-html-directive": "error"
+        // '@typescript-eslint/no-unused-vars': [
+        //   'error',
+        //   {argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_'},
+        // ]
+      },
+    },
+  ],
+  rules: {},
+};
